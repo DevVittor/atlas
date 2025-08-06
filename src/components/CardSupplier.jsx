db8d93a87f548c053cc301fe5ee5620502b63abe
DevVittor <vittorserradev@gmail.com>
2025-08-06 19:36:51 -0300

    Fix: Agora ficou mais fácil na hora de marcar o card do supplier

--- src/components/CardSupplier.jsx
+++ src/components/CardSupplier.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import { FaMapMarkerAlt } from "react-icons/fa";
-import { RiShieldUserFill } from "react-icons/ri";
-import { RiFireFill } from "react-icons/ri";
+import { RiShieldUserFill, RiFireFill } from "react-icons/ri";
 
 export default function CardSupplier() {
+  const [checked, setChecked] = useState(false);
+
+  const handleCardClick = () => {
+    setChecked(!checked);
+  };
+
+  const handleCheckboxClick = (e) => {
+    e.stopPropagation(); // Impede que o clique no checkbox também dispare o clique do card
+  };
+
   return (
-    <div className="flex justify-between items-start gap-2 rounded-lg hover:cursor-pointer hover:shadow-sm transition-shadow ease-in-out duration-300 border border-zinc-200 bg-white p-4 md:max-h-[150px]">
+    <div
+      className="flex justify-between items-start gap-2 rounded-lg hover:cursor-pointer hover:shadow-sm transition-shadow ease-in-out duration-300 border border-zinc-200 bg-white p-4 md:max-h-[150px]"
+      onClick={handleCardClick}
+    >
       <div className="flex items-start gap-3 overflow-auto">
         <div className="flex justify-center items-center flex-col gap-1.5">
           <div className="rounded-md h-[50px] w-[50px] border border-green-500 bg-green-300 flex justify-center items-center">
@@ -36,8 +49,13 @@ export default function CardSupplier() {
           </ol>
         </div>
       </div>
-      <div className="">
-        <input type="checkbox" name="" id="" />
+      <div>
+        <input
+          type="checkbox"
+          checked={checked}
+          onClick={handleCheckboxClick}
+          onChange={(e) => setChecked(e.target.checked)}
+        />
       </div>
     </div>
   );
